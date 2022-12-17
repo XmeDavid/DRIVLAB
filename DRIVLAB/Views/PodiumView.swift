25a9394ae35af411fe793a3aa819731c363559c0
Frederico <fredp.profissional@gmail.com>
2022-12-17 01:34:37 +0000

    First version of podium

--- DRIVLAB/Views/PodiumView.swift
+++ DRIVLAB/Views/PodiumView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct PodiumView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(
+            alignment: .top,
+            spacing: 10
+        ) {
+            ForEach(
+                1...3,
+                id: \.self
+            ) { el in
+                Group {
+                    if (el == 2)
+                     {
+                        VStack {
+                            Image(systemName: "crown.fill").foregroundColor(.yellow) .scaleEffect(2).offset(y: -10)
+                        Circle()
+                         .fill(.blue)
+                         .frame(width: 128, height: 128)
+                        }
+                     }
+                     else
+                     {
+                         ZStack{
+                             Image(systemName: "person.fill")
+                               .resizable()
+                               .frame(width: 32, height: 32)
+                               .foregroundColor(.white)
+                               .padding(20)
+                               .background(Color.green)
+                               .clipShape(Circle())
+                             Text(el == 1 ? "2" : el.description).offset(y:25)
+                         }.offset(y: 85)
+                     }
+               }
+            }
+        }
     }
 }
 
