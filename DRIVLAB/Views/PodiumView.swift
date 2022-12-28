e60bede4e7be052a700767af21d91df78a525c1e
Frederico <fredp.profissional@gmail.com>
2022-12-28 12:48:24 +0000

    Fix Podium View places

--- DRIVLAB/Views/PodiumView.swift
+++ DRIVLAB/Views/PodiumView.swift
@@ -20,63 +20,12 @@ struct PodiumView: View {
             alignment: .bottom,
             spacing: 10
         ) {
-            ForEach(
-                0...2,
-                id: \.self
-            ) { el in
-                Group {
-                    if (el == 1)
-                     {
-                        VStack {
-                            Image(systemName: "crown.fill").foregroundColor(.yellow) .scaleEffect(2).offset(y: -10)
-                            
-                            ZStack{
-                                
-                                Image("image")
-                                  .resizable()
-                                  .frame(width: 128, height: 128)
-                                  .overlay{
-                                      Circle().stroke(.white, lineWidth: 2)
-                                  }
-                                  .foregroundColor(.white)
-                                  .clipShape(Circle())
-                                Text(users[el].name.description + users[el].user_xp.description)
-                            }
-                        }
-                     }
-                    else if (el == 0){
-                        ZStack{
-                            Image("image")
-                              .resizable()
-                              .frame(width: 64, height: 64)
-                              .foregroundColor(.white)
-                              .clipShape(Circle())
-                              .overlay{
-                                  Image("2nd_place_overlay")
-                                      .resizable()
-                                      .frame(width: 72, height: 72)
-                              }
-                            Text(users[el].name.description + users[el].user_xp.description)
-                        }
-                        
-                    }else {
-                        ZStack{
-                            Image("image")
-                              .resizable()
-                              .frame(width: 64, height: 64)
-                              .foregroundColor(.white)
-                              .clipShape(Circle())
-                              .overlay{
-                                  Image("3rd_place_overlay")
-                                      .resizable()
-                                      .frame(width: 72, height: 72)
-                              }
-                            Text(users[el].name.description + users[el].user_xp.description)
-                        }
-                    }
-               }
-            }
-        }.onAppear{users = handleUsers(users: users)}
+        
+            FirstPlaceView(user: users[0])
+            SecondPlaceView(user: users[1])
+            ThirdPlaceView(user: users[2])
+            
+        }
     }
 }
 
@@ -86,18 +35,116 @@ struct PodiumView_Previews: PreviewProvider {
     }
 }
 
-func handleUsers(users: [User]) -> [User]{
+/*func handleUsers(users: [User]) -> [User]{
     
     var sortedUsers = users.sorted {
         $0.user_xp > $1.user_xp
     }
     
-    sortedUsers[1].user_xp += 50
-    sortedUsers[2].user_xp += 40
-    
     sortedUsers.swapAt(0, 1)
     
     return sortedUsers
+}*/
+
+
+struct FirstPlaceView: View {
+    var user: User
+    var body: some View {
+        VStack {
+            Image(systemName: "crown.fill").foregroundColor(.yellow) .scaleEffect(2).offset(y: -10)
+            
+            ZStack{
+                
+                Image("image")
+                  .resizable()
+                  .frame(width: 128, height: 128)
+                  .overlay{
+                      Circle().stroke(.white, lineWidth: 2)
+                  }
+                  .foregroundColor(.white)
+                  .clipShape(Circle())
+                Text(user.name + user.user_xp.description)
+            }
+        }
+    }
 }
 
+struct SecondPlaceView: View {
+    var user: User
+    var body: some View {
+        ZStack{
+            Image("image")
+              .resizable()
+              .frame(width: 64, height: 64)
+              .foregroundColor(.white)
+              .clipShape(Circle())
+              .overlay{
+                  Image("2nd_place_overlay")
+                      .resizable()
+                      .frame(width: 72, height: 72)
+              }
+            Text(user.name + user.user_xp.description)
+        }
+    }
+}
 
+struct ThirdPlaceView: View {
+    var user: User
+    var body: some View {
+        ZStack{
+            Image("image")
+              .resizable()
+              .frame(width: 64, height: 64)
+              .foregroundColor(.white)
+              .clipShape(Circle())
+              .overlay{
+                  Image("3rd_place_overlay")
+                      .resizable()
+                      .frame(width: 72, height: 72)
+              }
+            Text(user.name + user.user_xp.description)
+        }
+    }
+}
+
+/*ForEach(
+    0...2,
+    id: \.self
+) { el in
+    Group {
+        if (el == 1)
+         {
+            
+         }
+        else if (el == 0){
+            ZStack{
+                Image("image")
+                  .resizable()
+                  .frame(width: 64, height: 64)
+                  .foregroundColor(.white)
+                  .clipShape(Circle())
+                  .overlay{
+                      Image("2nd_place_overlay")
+                          .resizable()
+                          .frame(width: 72, height: 72)
+                  }
+                Text(users[el].name.description + users[el].user_xp.description)
+            }
+            
+        }else {
+            ZStack{
+                Image("image")
+                  .resizable()
+                  .frame(width: 64, height: 64)
+                  .foregroundColor(.white)
+                  .clipShape(Circle())
+                  .overlay{
+                      Image("3rd_place_overlay")
+                          .resizable()
+                          .frame(width: 72, height: 72)
+                  }
+                Text(users[el].name.description + users[el].user_xp.description)
+            }
+        }
+   }
+}*/
